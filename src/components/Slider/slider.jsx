c00acb5bdcbb05d5dde7e075e2f357ba5d998b88
pavel <pavel.polyakov0010@gmail.com>
2024-01-23 21:13:57 +0100

    Still slider

--- src/components/Slider/slider.jsx
+++ src/components/Slider/slider.jsx
@@ -11,19 +11,27 @@ const images = [
 
 export default function Slider(){
     const {currentImage, setCurrentImage} = useState(images[0]);
+    const {imageIndex, setImageIndex} = useState(0);
+    let arrows = "❮ ❯"; 
 
 
-    function setImage(){
+    function setImage(nextIndex){
+        if (imageIndex === 0 && nextIndex === -1) {
+            return;
+        }
+        if (imageIndex === images.length - 1 && nextIndex === 1) {
+            return;
+        }
 
-        setCurrentImage(images[0]);
+        setCurrentImage(imageIndex + nextIndex);
     }
 
     return (
         <div>
             <div className="flex relative mx-auto w-fit items-center">
-                <a href="#" onClick={setImage} className="absolute left-0 p-4 ml-2 rounded-md bg-white text-3xl font-bold">❮</a>
-                <img src={images[0]} width={600} height={600} />
-                <a href="#" onClick={setImage} className="absolute right-0 p-4 mr-2 rounded-md bg-white text-3xl font-bold">❯</a>
+                <a href="#" onClick={setImage(-1)} className="absolute left-0 p-4 ml-2 rounded-md bg-white text-3xl font-bold">U+276e</a>
+                <img src={currentImage} width={600} height={600} />
+                <a href="#" onClick={setImage(1)} className="absolute right-0 p-4 mr-2 rounded-md bg-white text-3xl font-bold">U+276f</a>
             </div>
             <SlideRow images={images} />
         </div>
